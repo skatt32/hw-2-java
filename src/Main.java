f877f29f289ff13beb41189901ead9e802044830
Neto test <test@neto.com>
2023-11-15 15:48:12 +0400

    cheange src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 18174; // стоимости билета
+
+        int oneBonusMile = 25; // одноа бонусная миля
+
+        System.out.println("Начисленные бонусные мили");
+        System.out.println(ticketPrice / oneBonusMile) ;
+
+    }
 }
